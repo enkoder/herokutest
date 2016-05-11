8f19084d94c83fc5dff735833be99b6efcb376b1
Kodie <kodiegoodwin@gmail.com>
2016-05-11 11:17:47 -0700

    Call python file from OneHandler

--- go/api/one.go
+++ go/api/one.go
@@ -2,13 +2,22 @@ package api
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
 )
 
 func OneHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		fmt.Fprintf(w, "Hello from one!")
+		f := filepath.Join(os.Getenv("GOPATH"), "src", "herokutest", "python.py")
+		out, err := exec.Command("python", f).Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(w, "Hello from one! Here is the output from python %s", string(out))
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
