39037f64061b3e4a68d719807d833293576da43b
Kodie <kodiegoodwin@gmail.com>
2016-05-11 11:31:30 -0700

    Add debug information

--- go/api/one.go
+++ go/api/one.go
@@ -11,10 +11,19 @@ import (
 func OneHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		f := filepath.Join(os.Getenv("GOPATH"), "src", "herokutest", "python.py")
+		cwd, err := os.Getwd()
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		gp := os.Getenv("GOPATH")
+		fmt.Fprintf(w, "Current Working DIR: %s\n", cwd)
+		fmt.Fprintf(w, "GOPATH = %s\n", cwd)
+		f := filepath.Join(gp, "src", "herokutest", "python.py")
 		out, err := exec.Command("python", f).Output()
 		if err != nil {
-			fmt.Fprintf(w, err.Error())
+			http.Error(w, err.Error(), 500)
+			return
 		}
 		fmt.Fprintf(w, "Hello from one! Here is the output from python %s", string(out))
 	default:
